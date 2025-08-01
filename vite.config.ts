7821da6e53a652d5a2f409c23501241af8cf32a1
SabarinathRajendran <sabarinathkrajenderan@gmail.com>
2025-08-02 01:29:49 +0530

    removed base url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/searchfortalentz",
+  // base: "/searchfortalentz",
   plugins: [
     react(),
     mode === 'development' &&
